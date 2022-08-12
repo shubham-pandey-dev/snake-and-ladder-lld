c6be7792c4f5d5851f838b89fe091f0045ae5705
Shubham Pandey <shubhampandey1810@gmail.com>
2022-08-12 23:50:03 +0530

    sp Add Player model class

---
+++ app/src/main/java/com/snake_and_ladder/model/Player.java
@@ -0,0 +1,19 @@
+package com.snake_and_ladder.model;
+
+public class Player {
+    private final String name;
+    private int position;
+
+    public Player(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
